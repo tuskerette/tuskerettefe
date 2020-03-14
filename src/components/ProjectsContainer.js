e723a3c294aed14ffd722f01d0e51bac23183dc0
tuskerette <valeria.graffeo@gmail.com>
2020-03-14 17:10:41 -0400

    add axios package

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class ProjectsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/projects.json')
+    .then(response => {
+      this.setState({projects: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+  
   render() {
     return (
       <div>
