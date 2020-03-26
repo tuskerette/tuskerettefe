274bfe81fff5a6acba683343a99aaea94ce70bd5
tuskerette <valeria.graffeo@gmail.com>
2020-03-25 22:04:58 -0400

    fix urls, add immutability-helper
    start building the add project post
    will be admin only.

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -19,14 +19,39 @@ class ProjectsContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewProject = (opts) => {
+   // opts = { project:
+     // { 
+       // title: '',
+       // github_link: '',
+       // app_link: '',
+       // description: '',
+       // date_created: ''
+      // }
+   // }
+
+    fetch('https://localhost:3001/api/v1/projects', {
+      method: 'post',
+      body: JSON.stringify(opts)
+    }).then(function(response) {
+      console.log(response); 
+      return response.json();
+    })
+    .catch(error => console.log(error))
+  }
 
   render() {
     return (
+    <div>
+      <button className="newProjectButton" onClick={this.addNewProject} >
+	Add a Project
+      </button>   
       <div>
-        {this.state.projects && this.state.projects.map((project) => {
-          return (<Project project={project} key={project.id} />)
-        })}
+          {this.state.projects && this.state.projects.map((project) => {
+            return (<Project project={project} key={project.id} />)
+          })}
       </div>
+    </div>
     );
   }
 }
