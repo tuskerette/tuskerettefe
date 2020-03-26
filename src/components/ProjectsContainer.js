1ab279dca600c225ad156ccdb84468f8b7c4818f
tuskerette <valeria.graffeo@gmail.com>
2020-03-25 20:05:05 -0400

    Getting data on the page, yay! and basic palette css

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -9,20 +9,32 @@ class ProjectsContainer extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/api/v1/projects.json')
-     .then(response => {
-      this.setState({projects: response.data})
+    return fetch('http://localhost:3001/api/v1/projects.json')
+    .then(response => response.json())
+    .then(response => {
+      this.setState({projects: response})
+      console.log(response);
     })
     .catch(error => console.log(error))
   }
-  
+
+
   render() {
     return (
       <div>
-        Projects
+	    {this.state.projects && this.state.projects.map((project) => {
+              return(
+                <div className="project-panel" key={project.id} >
+                  <h4>{project.title}</h4>
+	          <p>{project.app_link}</p>	  
+                  <p>{project.date_created}</p>
+                  <p>{project.github_link}</p>
+		  <p>{project.description}</p>
+		</div>
+              )       
+            })}
       </div>
-    )
+    );
   }
 }
-
 export default ProjectsContainer
