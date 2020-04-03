9d9a73d576685277fe037c095c6c29805a175b8f
tuskerette <valeria.graffeo@gmail.com>
2020-04-02 23:02:57 -0400

    going back to only get index. component is a proof of concept.
    will try to deploy.

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import Project from './Project'
-import axios from 'axios'
-import update from 'immutability-helper'
-import ProjectForm from './ProjectForm'
 
 class ProjectsContainer extends Component {
   constructor(props) {
@@ -17,54 +14,19 @@ class ProjectsContainer extends Component {
     .then(response => response.json())
     .then(response => {
       this.setState({projects: response})
-      // console.log(response);
     })
     .catch(error => console.log(error))
   }
 
-    addNewProject = () => {
-      axios.post(
-        'http://localhost:3001/api/v1/projects',
-        { project:
-          {
-            title: '',
-            app_link: '',
-	    github_link: '',
-	    description: '',
-	    date_created: ''
-          }
-        }
-      )
-      .then(response => {
-        console.log(response)
-        const projects = update(this.state.projects, {
-          $splice: [[0, 0, response.data]]
-        })
-        this.setState({
-	    projects: projects,
-	    editingProjectId: response.data.id
-	})
-      })
-      .catch(error => console.log(error))
-    }
-
-  render() {
-    return (
-    <div>
-      <button className="newProjectButton" onClick={this.addNewProject} >
-	Add a Project
-      </button>   
-      <div>
+    render() {
+      return (
+        <div>
           {this.state.projects && this.state.projects.map((project) => {
-            if(this.state.editingProjectId === project.id) {
-                return(<ProjectForm project={project} key={project.id} />)
-            } else {
-		return (<Project project={project} key={project.id} />)
-	    }
-	    })}
-      </div>
-    </div>
+              return (<Project project={project} key={project.id} />)
+        })}
+	</div>
     );
   }
 }
+
 export default ProjectsContainer
