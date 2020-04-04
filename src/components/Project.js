89059a65e5e0acabedd395dd4b2bdb33f27f3c8c
tuskerette <valeria.graffeo@gmail.com>
2020-04-04 12:45:40 -0400

    proxy done right and fix external links

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Project = ({project}) =>
   <div className="project-panel" key={project.id}>
     <h4>{project.title}</h4>
-    <p><a href={project.app_link} target="_blank">live demo</a></p>
-    <p><a href={project.github_link} target="_blank">repo</a></p>
+    <p><a href={project.app_link} target="_blank" rel="noopener">live demo</a></p>
+    <p><a href={project.github_link} target="_blank" rel="noopener">repo</a></p>
     <p>{project.description}</p>
     <p>{project.date_created}</p>
   </div>
