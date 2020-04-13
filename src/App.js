f021cde141cf1edb0ad8532c3357331a0a99c061
tuskerette <valeria.graffeo@gmail.com>
2020-04-13 19:18:49 -0400

    p-an setup and initial css cleanup

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import ProjectsContainer from './components/ProjectsContainer'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Projects</h1>
+      <div className='App'>
+        <div className='static-text'>
+	  <h1>Tuskerette</h1>
+	  <p>Hi, I am Valeria Graffeo and this is my personal website/portfolio.</p>
+	  <p>I am a full-stack developer currently based in the Boston area, and working primarily on Ruby on Rails,
+	  but I like to call myself a programmer, well and Valeria, too. Except I tend not call myself, usually others call me.</p>
+	  <p>I work every day towards my goal of becoming a developer who crafts real good code, effective but also with creative solutions, across the stack and across the US or Europe, learning every day.</p>
         </div>
-        <ProjectsContainer />
+	<ProjectsContainer />
       </div>
     );
   }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,12 @@
-const proxy = require('http-proxy-middleware');
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const env_url = ((process.env.NODE_ENV === 'production') ? 'https://tuskerette.herokuapp.com' : 'http://localhost:3001');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: env_url }));
+    app.use(
+        '/api',
+	createProxyMiddleware({
+	    target: env_url,
+	    changeOrigin: true
+	})
+    );
 };
